74ef09317309b4ec6467091213ec76530a9b4f2a
gopal <swamymushini@gmail.com>
2021-11-30 22:11:56 +0530

    gopal

--- MovieTicketBooking/src/main/java/com/capg/mtb/exceptions/BookingNotFoundException.java
+++ MovieTicketBooking/src/main/java/com/capg/mtb/exceptions/BookingNotFoundException.java
@@ -4,10 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="id is not available")
 public class BookingNotFoundException extends Exception {
-/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 public BookingNotFoundException()
 {
 	super();
